c0bf76bdf81e2f95afe43c378a50189c24337d70
assertivist <andy.halstead@gmail.com>
2015-09-14 08:18:56 +0000

    logging in / signing up mostly done

--- client/app.ng.js
+++ client/app.ng.js
@@ -32,10 +32,31 @@ angular.module('speakeasy').controller('InputCtrl', function($scope, $meteor) {
 });
 
 angular.module('speakeasy').controller('SignupLoginCtrl', function($scope, $meteor) {
-  $scope.signup = function (user_obj) { 
-    
+  $scope.signup = function (user_obj) {
+    Accounts.createUser({
+      email: user_obj.email,
+      password: user_obj.password,
+      username: user_obj.username
+    }, function(err) {
+      if (err) {
+        Session.set('displayMessage', 'Error  - There was an error creating your account.');
+      }
+      else {
+        $('#login-modal').foundation('reveal','close');
+      }
+    })
   };
-  $scope.login = function (user_obj) { };
+  $scope.login = function (user_obj) {
+    Meteor.loginWithPassword(user_obj.nameoremail, user_obj.password, function(err) {
+      if (err) {
+        Session.set('displayMessage', 'Login failed - Please check username/password.');
+      }
+      else {
+        //login success
+        $('#login-modal').foundation('reveal','close');
+      }
+    });
+  };
 });
 
 angular.module('speakeasy').directive('enterPressed', function () {
@@ -65,4 +86,27 @@ Template.loginButtons.rendered = function () {
 
 Accounts.ui.config({
    passwordSignupFields: "USERNAME_AND_OPTIONAL_EMAIL"
+});
+
+Meteor.autorun(function() {
+  // Whenever this session variable changes, run this function.
+  var message = Session.get('displayMessage');
+  if (message) {
+    sAlert.error(message);
+    Session.set('displayMessage', null);
+  }
+});
+
+sAlert.config({
+  position: 'top-right',
+  timeout: 5000,
+  html: false,
+  onRouteClose: true,
+  stack: true,
+  // or you can pass an object:
+  // stack: {
+  //     spacing: 10 // in px
+  //     limit: 3 // when fourth alert appears all previous ones are cleared
+  // }
+  offset: 0, // in px - will be added to first alert (bottom or top - depends of the position in config)
 });
